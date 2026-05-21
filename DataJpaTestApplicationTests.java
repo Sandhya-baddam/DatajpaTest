d1009b7c263edfb130ffaa8f632b16e26ef60495
Sandhya-baddam <baddamsandhya2000@gmail.com>
2026-05-21 15:36:19 +0530

    Add files via upload
    added files

---
+++ DataJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataJpaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
